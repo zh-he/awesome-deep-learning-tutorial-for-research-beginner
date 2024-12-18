bb19ca1426e8cd344cdc1448ebf8b792b0e2cba7
zh-he <331848114@qq.com>
2024-12-18 18:27:32 +0800

    feat:Add a CNN directory

---
+++ CNN/ResNet.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+""""
+    Author: Zhihai He
+    Date: 2024/12/18
+    Data Source: 链接：https://pan.baidu.com/s/1l1AnBgkAAEhh0vI5_loWKw 提取码：2xq4 blog:https://blog.csdn.net/dddccc1234/article/details/122622182
+    Description: 利用ResNet实现猫狗分类
+    Have Done: ResNet sturcture
+    To Do: Load data, Classify
+"""
+
+# 残差块
+class Residual(nn.Module):
+    def __init__(self, input_channels, num_channels, strides=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, num_channels, kernel_size=3, stride=strides, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_channels)
+        self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides) if strides != 1 or input_channels != num_channels else None
+
+    def forward(self, x):
+        y = F.relu(self.bn1(self.conv1(x)))
+        y = self.bn2(self.conv2(y))
+        x = self.conv3(x) if self.conv3 else x
+        return F.relu(x + y)
+
+class SimpleResNet(nn.Module):
+    def __init__(self, input_channels, num_channels, num_classes=2):
+        super(SimpleResNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(input_channels, num_channels, kernel_size=7, stride=1, padding=1),
+            nn.BatchNorm2d(num_channels),
+            nn.ReLU(),
+            self._make_layer(num_channels, num_channels * 2, blocks=2),
+            self._make_layer(num_channels * 2, num_channels * 4, blocks=2),
+            self._make_layer(num_channels * 4, num_channels * 8, blocks=2),
+            self._make_layer(num_channels * 8, num_channels * 16, blocks=2),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten()
+        )
+        self.classifier = nn.Linear(num_channels * 16, num_classes)
+
+    # *将List转为nn.Moudle对象
+    def _make_layer(self, input_channels, num_channels, blocks, stride=2):
+        layers = []
+        layers.append(Residual(input_channels, num_channels, strides=stride))
+        for _ in range(1, blocks):
+            layers.append(Residual(num_channels, num_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
